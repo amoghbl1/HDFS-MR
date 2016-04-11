78b317df33627d4638c29f9404b7025d47339cd6
Amogh Pradeep <amoghbl1@gmail.com>
2016-04-11 14:28:32 +0530

    JT expanded to create thread for a job and monitor the thread on getStatus call with a JID

--- Jobtracker/source/JobTracker.java
+++ Jobtracker/source/JobTracker.java
@@ -1,3 +1,5 @@
+import com.distributed.systems.MRProtos.HeartBeatRequest;
+import com.distributed.systems.MRProtos.HeartBeatResponse;
 import com.distributed.systems.MRProtos.JobStatusRequest;
 import com.distributed.systems.MRProtos.JobStatusResponse;
 import com.distributed.systems.MRProtos.JobSubmitRequest;
@@ -5,15 +7,36 @@ import com.distributed.systems.MRProtos.JobSubmitResponse;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 public class JobTracker extends UnicastRemoteObject implements JobTrackerInterface {
 
     // Using this default config file
     private static String configFile = "hdfs_mr_job_tracker.conf";
 
+    private static int currentJobID = 0;
+
+    private static HashMap<Integer, JobRunnerThread> currentJobThreads = new HashMap<Integer, JobRunnerThread>();
+
+
+    private final Object CJTLock = new Object();
 
     public JobTracker(String conf) throws RemoteException {
         this.configFile = conf;
+    }
+
+    public synchronized int getAndIncrementJobID() {
+        this.currentJobID += 1;
+        return this.currentJobID - 1;
+    }
+
+    public void removeJobRunnerFromJRList(int id) {
+        synchronized(CJTLock) {
+            if(currentJobThreads.containsKey(id)) {
+                currentJobThreads.remove(id);
+                System.out.println("Job Completed!! JID: " + id);
+            }
+        }
     }
 
     /* JobSubmitResponse jobSubmit(JobSubmitRequest) */
@@ -24,9 +47,22 @@ public class JobTracker extends UnicastRemoteObject implements JobTrackerInterfa
             JobSubmitRequest jobSubmitRequest = JobSubmitRequest.parseFrom(encodedRequest);
             System.out.println(jobSubmitRequest.toString());
 
+            JobRunnerThread jrt = new JobRunnerThread(this,
+                    jobSubmitRequest.getMapperName(),
+                    jobSubmitRequest.getReducerName(),
+                    jobSubmitRequest.getInputFile(),
+                    jobSubmitRequest.getOutputFile(),
+                    jobSubmitRequest.getNumReduceTasks());
+            jrt.start();
+            synchronized(CJTLock) {
+                currentJobThreads.put(
+                        new Integer(jrt.getJID()),
+                        jrt);
+            }
+
             jobSubmitResponseBuilder.setStatus(1);
             // Queue job and get job ID and then setting job ID.
-            jobSubmitResponseBuilder.setJobId(0);
+            jobSubmitResponseBuilder.setJobId(jrt.getJID());
         } catch (Exception e) {
             System.out.println("Problem parsing job submit request??" + e.getMessage());
             e.printStackTrace();
@@ -43,11 +79,18 @@ public class JobTracker extends UnicastRemoteObject implements JobTrackerInterfa
             System.out.println(jobStatusRequest.toString());
 
             jobStatusResponseBuilder.setStatus(1);
-            jobStatusResponseBuilder.setJobDone(true);
-            jobStatusResponseBuilder.setTotalMapTasks(0);
-            jobStatusResponseBuilder.setNumMapTasksStarted(0);
-            jobStatusResponseBuilder.setTotalReduceTasks(0);
-            jobStatusResponseBuilder.setNumReduceTasksStarted(0);
+            synchronized(CJTLock) {
+                if(currentJobThreads.containsKey(jobStatusRequest.getJobId())) {
+                    jobStatusResponseBuilder.setJobDone(false);
+                    jobStatusResponseBuilder.setTotalMapTasks(0);
+                    jobStatusResponseBuilder.setNumMapTasksStarted(0);
+                    jobStatusResponseBuilder.setTotalReduceTasks(0);
+                    jobStatusResponseBuilder.setNumReduceTasksStarted(0);
+                }
+                else {
+                    jobStatusResponseBuilder.setJobDone(true);
+                }
+            }
         } catch (Exception e) {
             System.out.println("Problem parsing job status request??" + e.getMessage());
             e.printStackTrace();
@@ -58,6 +101,14 @@ public class JobTracker extends UnicastRemoteObject implements JobTrackerInterfa
     /* HeartBeatResponse heartBeat(HeartBeatRequest) */
     public byte[] heartBeat(byte[] encodedRequest) throws RemoteException {
         System.out.println("Received heart beat!!");
+        HeartBeatRequest heartBeatRequest = null;
+        try {
+            heartBeatRequest = HeartBeatRequest.parseFrom(encodedRequest);
+            System.out.println(heartBeatRequest.toString());
+        } catch (Exception e) {
+            System.out.println("Problem parsing heart beat request??" + e.getMessage());
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -71,5 +122,45 @@ public class JobTracker extends UnicastRemoteObject implements JobTrackerInterfa
             e.printStackTrace();
         }
         System.out.println("Job Tracker bound to RMI Registry!! ");
+    }
+
+
+    public class JobRunnerThread extends Thread {
+
+        private JobTracker parentJT;
+        private String mapperName = "";
+        private String reducerName = "";
+        private String inputFile = "";
+        private String outputFile = "";
+        private int numberOfReducers = 0;
+
+        private int JID = 0;
+
+        public JobRunnerThread(JobTracker parent, String mapper, String reducer, String input, String output, int numberReducers) {
+            // A new Job has been started with the following parameters
+            // A thread has been spawned to handle this job
+            // Updates to this job can be monitored using a function getStatus()
+            this.parentJT = parent;
+            this.mapperName = mapper;
+            this.reducerName = reducer;
+            this.inputFile = input;
+            this.outputFile = output;
+            this.numberOfReducers = numberReducers;
+
+            this.JID = this.parentJT.getAndIncrementJobID();
+        }
+
+        public String getStatus() {
+            return "";
+        }
+
+        public int getJID() {
+            return this.JID;
+        }
+
+        public void run() {
+            try { Thread.sleep(10000); } catch (Exception e){}
+            this.parentJT.removeJobRunnerFromJRList(this.JID);
+        }
     }
 }
